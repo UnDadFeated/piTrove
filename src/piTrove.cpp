16711817709c0f6b7908e20eb163f1d8ce17fe9c
UnDadFeated <UnDadFeated@piTrove>
2026-05-20 10:48:00 -0700

    v8.0.2: Fix preload_running never resetting, bump version

--- src/piTrove.cpp
+++ src/piTrove.cpp
@@ -10,7 +10,7 @@
  *   • Slideshow – raylib, preload, crossfade, Ken Burns
  */
 
-#define VERSION "8.0.0"
+#define VERSION "8.0.2"
 #define APP_NAME "piTrove"
 
 // Global atomics for headless features
