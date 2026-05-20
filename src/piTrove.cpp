b3b2961a34542652f961667c775667d05f7cf694
UnDadFeated <UnDadFeated@piTrove>
2026-05-19 22:56:57 -0700

    Bump version to 7.1.7

--- src/piTrove.cpp
+++ src/piTrove.cpp
@@ -10,7 +10,7 @@
  *   • Slideshow – raylib, preload, crossfade, Ken Burns
  */
 
-#define VERSION "7.1.1"
+#define VERSION "7.1.7"
 #define APP_NAME "piTrove"
 
 // Global atomics for headless features
