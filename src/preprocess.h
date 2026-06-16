58623ad12e890ac2fcc5704f8e4b50b24ce865a3
UnDadFeated <UnDadFeated@piTrove>
2026-06-16 10:13:11 -0700

    Cache: Implement background media metadata preprocessing worker thread (v13.0.8)

---
+++ src/preprocess.h
@@ -0,0 +1,7 @@
+#ifndef PITROVE_PREPROCESS_H
+#define PITROVE_PREPROCESS_H
+
+void start_preprocess_worker();
+void stop_preprocess_worker();
+
+#endif // PITROVE_PREPROCESS_H
--- src/util.h
+++ src/util.h
@@ -1,7 +1,7 @@
 #ifndef PITROVE_UTIL_H
 #define PITROVE_UTIL_H
 
-#define VERSION "13.0.7"
+#define VERSION "13.0.8"
 #define APP_NAME "piTrove"
 
 #include <atomic>
